6b544967951d865eebd0a0f880bbeb1581a62746
radithya-mahesa <radithya.syabil@gmail.com>
2025-04-30 22:13:42 +0700

    radit|30042025|refactoring code

--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -37,13 +37,12 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
       className="w-full bg-white dark:bg-neutral-950 font-sans md:px-10"
       ref={containerRef}
     >
-      <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
-        <h2 className="text-lg md:text-4xl mb-4 text-black dark:text-white max-w-4xl">
-          Changelog from my journey
+      <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10 text-center">
+        <h2 className="text-3xl md:text-4xl mb-4 font-bold text-black dark:text-white">
+          Garis waktu <span className="text-sky-500">perjalanan kami</span>🚀
         </h2>
-        <p className="text-neutral-700 dark:text-neutral-300 text-sm md:text-base max-w-sm">
-          I&apos;ve been working on Aceternity for the past 2 years. Here&apos;s
-          a timeline of my journey.
+        <p className="text-neutral-700 dark:text-neutral-300 text-md md:text-lg max-w-2xl mx-auto">
+         <span className="text-neutral-500 font-bold">KITA</span> telah melakukan banyak hal yang luar biasa dalam perjalanan ini. Pada akhirnya, kita telah berhasil mencapai titik akhir bersama-sama. Cheers 🍻
         </p>
       </div>
 
@@ -54,16 +53,16 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
             className="flex justify-start pt-10 md:pt-40 md:gap-10"
           >
             <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
-              <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-white dark:bg-black flex items-center justify-center">
-                <div className="h-4 w-4 rounded-full bg-neutral-200 dark:bg-neutral-800 border border-neutral-300 dark:border-neutral-700 p-2" />
+              <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-transparent dark:bg-black flex items-center justify-center">
+                <div className="h-4 w-4 rounded-full bg-sky-500 border border-sky-600 p-2" />
               </div>
-              <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-neutral-500 dark:text-neutral-500 ">
+              <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold dark:text-neutral-500 ">
                 {item.title}
               </h3>
             </div>
 
-            <div className="relative pl-20 pr-4 md:pl-4 w-full">
-              <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-500 dark:text-neutral-500">
+            <div className="relative italic rounded-lg mx-3 md:mx-auto bg-white p-2 pt-6 border-2 hover:border-sky-500 pl-20 pr-6 md:pt-8 md:pr-8 md:pl-8 w-full">
+              <h3 className="md:hidden block text-2xl mb-4 text-left font-bold dark:text-neutral-500">
                 {item.title}
               </h3>
               {item.content}{" "}
